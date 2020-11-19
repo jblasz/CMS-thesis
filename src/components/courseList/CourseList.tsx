488834c5520a9ba9a87e02d35a651284d5dd5b28
kblasz <jakub.blaszkowski@onemeter.com>
2020-11-19 23:15:11 +0100

    course tiles nonsense

--- src/components/courseList/CourseList.tsx
+++ src/components/courseList/CourseList.tsx
@@ -1,167 +1,152 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Card, Container, ListGroup, CardDeck, InputGroup, FormControl, ButtonGroup, Button,
+  ButtonGroup, Button,
 } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfo } from '@fortawesome/free-solid-svg-icons';
 import Dropdown from 'react-dropdown';
-import { Course } from '../../interfaces/course';
+import { Course, CourseLanguage } from '../../interfaces/course';
 import 'react-dropdown/style.css';
+import './CourseList.css';
 
 export interface CourseListComponentProps {
   courses: Course[];
+}
+
+enum Language {
+  ANY = 0,
+  PL,
+  EN
+}
+
+enum Status {
+  ANY = 0,
+  OPEN,
+  CLOSED
 }
 
 function CourseListComponent(props: CourseListComponentProps): JSX.Element {
   const [t] = useTranslation();
   const { courses } = props;
 
-  const [languageFilter, setLanguageFilter] = useState('');
+  const [languageFilter, setLanguageFilter] = useState(Language.ANY);
+  const [statusFilter, setStatusFilter] = useState(Status.OPEN);
   const [semesterFilter, setSemesterFilter] = useState('');
+
+  const semesters = ['any', ...new Set(courses.map((c) => c.semester).sort().reverse())];
 
   return (
     <section className="">
       <div className="site-content">
         <div className="posts">
-          <div className="post-content">
-            <div className="post-info">
-              <span>
-                <FontAwesomeIcon icon={faInfo} />
-              </span>
+          <section className="details-card">
+            <div className="row justify-content-center">
+              {courses.map((course) => (
+                <div className="card col-6 px-3">
+                  <div className="card-content">
+                    <div className="card-img">
+                      <img src="https://placeimg.com/380/230/nature" alt="" />
+                      <span><h4>{course.semester}</h4></span>
+                    </div>
+                    <div className="card-desc">
+                      <h3>{course.name}</h3>
+                      <p>
+                        {course.description}
+                      </p>
+                      <Link className="btn-card" to={`/courses/${course._id}`}>{t('COURSE.LABORATORY.GOTO')}</Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-          </div>
+          </section>
         </div>
-        <aside className="sidebar">
+        <aside className="sidebar mx-4">
           <div className="my-2">
             <div className="my-2">
               <ButtonGroup>
-                <Button className={`shadow-none button ${!languageFilter ? 'activated' : 'inactive'}`}>
-                  any
+                <Button
+                  className="shadow-none button"
+                  active={languageFilter === Language.ANY}
+                  onClick={() => setLanguageFilter(Language.ANY)}
+                >
+                  *
                 </Button>
-                <Button className="button">
+                <Button
+                  className="shadow-none button"
+                  active={languageFilter === Language.PL}
+                  onClick={() => setLanguageFilter(Language.PL)}
+                >
                   pl
                 </Button>
-                <Button className="button">
+                <Button
+                  className="shadow-none button"
+                  active={languageFilter === Language.EN}
+                  onClick={() => setLanguageFilter(Language.EN)}
+                >
                   en
                 </Button>
               </ButtonGroup>
             </div>
             <div className="my-2">
               <ButtonGroup>
-                <Button className={`shadow-none button ${!languageFilter ? 'activated' : 'inactive'}`}>
-                  any
+                <Button
+                  className="shadow-none button"
+                  active={statusFilter === Status.ANY}
+                  onClick={() => setStatusFilter(Status.ANY)}
+                >
+                  *
                 </Button>
-                <Button className="button">
-                  open
+                <Button
+                  className="shadow-none button"
+                  active={statusFilter === Status.OPEN}
+                  onClick={() => setStatusFilter(Status.OPEN)}
+                >
+                  ongoing
                 </Button>
-                <Button className="button">
+                <Button
+                  className="shadow-none button"
+                  active={statusFilter === Status.CLOSED}
+                  onClick={() => setStatusFilter(Status.CLOSED)}
+                >
                   closed
                 </Button>
               </ButtonGroup>
             </div>
             <div className="my-2" style={{ maxWidth: 250 }}>
-              <Dropdown options={['oa', 'o b c', 'd e fdfs']} />
+              <Dropdown
+                options={semesters}
+                value={semesterFilter}
+                onChange={(event) => {
+                  setSemesterFilter(event.value);
+                }}
+              />
             </div>
           </div>
           <div className="category">
             <h2>Courses</h2>
             <ul className="category-list">
-              <li className="list-items">
-                <Link to="/">Course 1</Link>
-                <span>2020Z</span>
-              </li>
-              <li className="list-items">
-                <Link to="/">Course 2</Link>
-                <span>2020Z</span>
-              </li>
-              <li className="list-items">
-                <Link to="/">Course 3</Link>
-                <span>2020Z</span>
-              </li>
-              <li className="list-items">
-                <Link to="/">Course 4</Link>
-                <span>2020Z</span>
-              </li>
+              {courses
+                .filter((course) => (semesterFilter ? course.semester === semesterFilter : true))
+                .filter((course) => (languageFilter
+                  ? (course.language === CourseLanguage.EN && languageFilter === Language.EN)
+                    || (course.language === CourseLanguage.PL && languageFilter === Language.PL)
+                  : true))
+                .filter((course) => (statusFilter
+                  ? (course.active && statusFilter === Status.OPEN)
+                    || (!course.active && statusFilter === Status.CLOSED)
+                  : true))
+                .map((course) => (
+                  <li className="list-items">
+                    <span>{course.name}</span>
+                    <span>{course.semester}</span>
+                  </li>
+                ))}
             </ul>
           </div>
         </aside>
       </div>
     </section>
-  );
-
-  return (
-    <Container>
-      <InputGroup className="mb-3">
-        <InputGroup.Prepend>
-          <InputGroup.Text>
-            {t('COURSE.FILTER.LANGUAGE')}
-          </InputGroup.Text>
-          <FormControl
-            as="select"
-            placeholder={t('COURSE.FILTER.LANGUAGE')}
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onChange={(e: any) => setLanguageFilter(e.target.value)}
-          >
-            {
-              ['', ...new Set<string>(courses.map((c) => c.language))]
-                .map((language) => (<option key={language}>{language}</option>))
-            }
-          </FormControl>
-          <InputGroup.Text>
-            {t('COURSE.FILTER.SEMESTER')}
-          </InputGroup.Text>
-          <FormControl
-            as="select"
-            placeholder={t('COURSE.FILTER.SEMESTER')}
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onChange={(e: any) => setSemesterFilter(e.target.value)}
-          >
-            {
-              ['', ...new Set<string>(courses.map((c) => c.semester))]
-                .map((semester) => (<option key={semester}>{semester}</option>))
-            }
-          </FormControl>
-        </InputGroup.Prepend>
-      </InputGroup>
-      <CardDeck>
-        {courses
-          .filter(
-            (course) => course.language.includes(languageFilter)
-              && course.semester.includes(semesterFilter),
-          )
-          .map((course) => (
-            <Card className="mb-4" style={{ minWidth: '350px', maxWidth: '350px' }} key={course._id}>
-              <Card.Body>
-                <Card.Title>{course.name}</Card.Title>
-                <Card.Text>{course.description}</Card.Text>
-                <ListGroup>
-                  <ListGroup.Item>
-                    {`${t('COURSE.CONDUCTED_IN_LANGUAGE')}: ${
-                      course.language
-                    }`}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    {`${t('COURSE.SEMESTER')}: ${course.semester}`}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    {`${t('COURSE.LABORATORIES_DEFINED')}: ${course.laboratories.length}`}
-                  </ListGroup.Item>
-                </ListGroup>
-                <Link
-                  to={`courses/${course._id}`}
-                  className="mt-2 btn btn-primary btn-lg active"
-                  role="button"
-                  aria-pressed="true"
-                >
-                  {t('COURSE.LABORATORY.GOTO')}
-                </Link>
-              </Card.Body>
-            </Card>
-          ))}
-      </CardDeck>
-    </Container>
   );
 }
 
